c45b05f0ce9cc825f94e293639de2f30e5231012
gw67-alt <gw67656@gmail.com>
2025-04-06 14:24:19 +1000

    Update lpc-word-auto.py

--- lpc-word-auto.py
+++ lpc-word-auto.py
@@ -323,11 +323,9 @@ def categorize_words(words_to_process, num_clusters=8, use_comprehensive=True, c
 # --- Main function ---
 def main():
     input_word_file = "words_alpha.txt"
-    
-    if input("(u)ser input, (f)ile? ") == "f":
-
-
-        with open(input("Enter filename: "), 'r', encoding='utf-8') as file:
+    filename = input("Enter filename or leave blank: ")
+    if len(filename) > 0:
+        with open(filename, 'r', encoding='utf-8') as file:
             all_words = file.read().upper().split()
     else:
 
